967aed593cd1201e49ae6f7283a3d11cc7c2cc57
w11th <w.eleventh@gmail.com>
2016-09-20 11:55:20 +0800

    Implement advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select "div.alert"
     assert_not flash.empty?
-    assert logged_in?
+    assert is_logged_in?
   end
 end
