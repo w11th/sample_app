0d384277c517d6a9f34c4e65d4b263cc6ec4a350
w11th <w.eleventh@gmail.com>
2016-09-24 14:17:24 +0800

    Add account activations improve performance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -60,7 +61,7 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to root_url unless current_user?(@user)
     end
 
     def admin_user
---
+++ test/integration/users_show_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersShowTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = users(:malory)
+  end
+
+  test "should redirect when show in-activated user profile" do
+    get user_path(@user)
+    assert_redirected_to root_url
+  end
+
+end
